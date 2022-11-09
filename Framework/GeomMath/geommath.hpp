55ba200c326af5dca78e3b4472cc38571473874e
playmaker <1344085659@qq.com>
2022-11-09 10:00:41 +0800

    fix bug

--- Framework/GeomMath/geommath.hpp
+++ Framework/GeomMath/geommath.hpp
@@ -4,7 +4,6 @@
 #include <limits>
 #include <math.h>
 #include "include/CrossProduct.h"
-#include "include/DotProduct.h"
 #include "include/MulByElement.h"
 #include "include/Normalize.h"
 #include "include/Transform.h"
@@ -142,7 +141,10 @@ namespace ZetaEngine {
         operator const T*() const { return static_cast<const T*>(data); };
         Vector4Type& operator=(const T* f) 
         { 
-            memcpy(data, f, sizeof(T) * 4); 
+            for (int32_t i = 0; i < 4; i++)
+            {
+                data[i] = *(f + i); 
+            }
             return *this;
         };
         
@@ -201,10 +203,25 @@ namespace ZetaEngine {
         ispc::CrossProduct(vec1, vec2, result);
     }
 
+    template <typename T>
+    inline void DotProduct(T& result, const T* a, const T* b, size_t count)
+    {
+        T* _result = new T[count];
+
+        result = static_cast<T>(0);
+
+        ispc::MulByElement(a, b, _result, count);
+        for (size_t i = 0; i < count; i++) {
+            result += _result[i];
+        }
+
+        delete[] _result;
+    }
+
     template <template <typename> class TT, typename T>
     inline void DotProduct(T& result, const TT<T>& vec1, const TT<T>& vec2)
     {
-        ispc::DotProduct(vec1, vec2, &result, countof(vec1.data));
+        DotProduct(result, static_cast<const T*>(vec1), static_cast<const T*>(vec2), countof(vec1.data));
     }
 
     template <typename T>
@@ -236,11 +253,16 @@ namespace ZetaEngine {
 
         Matrix& operator=(const T* _data) 
         {
-            memcpy(data, _data, ROWS * COLS * sizeof(T));
+            for (int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLS; j++) {
+                    data[i][j] = *(_data + i * COLS + j);
+                }
+            }
             return *this;
         }
     };
 
+    typedef Matrix<float, 3, 3> Matrix3X3f;
     typedef Matrix<float, 4, 4> Matrix4X4f;
 
     template <typename T, int ROWS, int COLS>
@@ -294,7 +316,8 @@ namespace ZetaEngine {
         Transpose(matrix2_transpose, matrix2);
         for (int i = 0; i < Da; i++) {
             for (int j = 0; j < Dc; j++) {
-                ispc::DotProduct(matrix1[i], matrix2_transpose[j], &result[i][j], Db);
+                DotProduct(result[i][j], matrix1[i], matrix2_transpose[j], Db);
+
             }
         }
 
@@ -316,10 +339,13 @@ namespace ZetaEngine {
         ispc::Transpose(matrix1, result, ROWS, COLS);
     }
 
-    template <typename T>
-    inline void Normalize(T& result)
+    template <template <typename> class TT, typename T>
+    inline void Normalize(TT<T>& a)
     {
-        ispc::Normalize(result, countof(result.data));
+        T length;
+        DotProduct(length, static_cast<T*>(a), static_cast<T*>(a), countof(a.data));
+        length = sqrt(length);
+        ispc::Normalize(a, length, countof(a.data));
     }
 
     inline void MatrixRotationYawPitchRoll(Matrix4X4f& matrix, const float yaw, const float pitch, const float roll)
@@ -520,7 +546,7 @@ namespace ZetaEngine {
         Matrix4X4f rotation = {{{
             {   1.0f - 2.0f * q.y * q.y - 2.0f * q.z * q.z,  2.0f * q.x * q.y + 2.0f * q.w * q.z,   2.0f * q.x * q.z - 2.0f * q.w * q.y,    0.0f    },
             {   2.0f * q.x * q.y - 2.0f * q.w * q.z,    1.0f - 2.0f * q.x * q.x - 2.0f * q.z * q.z, 2.0f * q.y * q.z + 2.0f * q.w * q.x,    0.0f    },
-            {   2.0f * q.x * q.z + 2.0f * q.w * q.y,    2.0f * q.y * q.z - 2.0f * q.y * q.z - 2.0f * q.w * q.x, 1.0f - 2.0f * q.x * q.x - 2.0f * q.y * q.y, 0.0f    },
+            {   2.0f * q.x * q.z + 2.0f * q.w * q.y,    2.0f * q.y * q.z - 2.0f * q.w * q.x, 1.0f - 2.0f * q.x * q.x - 2.0f * q.y * q.y, 0.0f    },
             {   0.0f,   0.0f,   0.0f,   1.0f    }
         }}};
 
