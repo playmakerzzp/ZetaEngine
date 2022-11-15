0ada50b4ddd6014cd70bc6dbe34ee74df41af1ea
playmaker <1344085659@qq.com>
2022-11-15 17:14:25 +0800

    add test resource

--- Asset/Shaders/cbuffer2.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:41d7eb78465f22d01624f9623a7bbd2d79f6edddb8c07042898e2a8790a257b1
-size 434
--- Asset/Shaders/vsoutput2.hs
+++ Asset/Shaders/vsoutput2.hs
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b7206ff3f48a0e4f284b438fa04bc7ec950072c91216f698ed0c7a17465c8522
-size 257
+#ifndef __VSOUTPUT_H__
+#define __VSOUTPUT_H__
+
+struct v2p
+{
+    float4 Position     : SV_POSITION;
+    float2 TextureUV    : TEXCOORD0;
+	float3 vNorm		: TEXCOORD1;
+    float4 vTang		: TEXCOORD2;
+	float3 vPosInView	: TEXCOORD3;
+};
+
+
+
+#endif
+
