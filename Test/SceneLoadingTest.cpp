fa400662d363cb290c4070c2666d43e18ed162d6
playmaker <1344085659@qq.com>
2022-11-16 11:15:40 +0800

    add test

--- Test/SceneLoadingTest.cpp
+++ Test/SceneLoadingTest.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "AssetLoader.hpp"
 #include "MemoryManager.hpp"
-#include "OGEX.hpp"
+#include "SceneManager.hpp"
 
 using namespace ZetaEngine;
 using namespace std;
@@ -10,6 +10,7 @@ using namespace std;
 namespace ZetaEngine {
     MemoryManager* g_pMemoryManager = new MemoryManager();
     AssetLoader* g_pAssetLoader = new AssetLoader();
+    SceneManager* g_pSceneManager = new SceneManager();
 }
 
 template<typename T>
@@ -26,37 +27,53 @@ static ostream& operator<<(ostream& out, unordered_map<string, shared_ptr<T>> ma
 int main(int, char**)
 {
     g_pMemoryManager->Initialize();
+    g_pSceneManager->Initialize();
     g_pAssetLoader->Initialize();
 
-    string ogex_text = g_pAssetLoader->SyncOpenAndReadTextFileToString("Scene/Example.ogex");
-
-    OgexParser* ogex_parser = new OgexParser();
-    unique_ptr<Scene> pScene = ogex_parser->Parse(ogex_text);
-    delete ogex_parser;
-
-    cout << "Dump of Scene Graph" << endl;
-    cout << "---------------------------" << endl;
-    cout << *pScene->SceneGraph << endl;
+    g_pSceneManager->LoadScene("Scene/cube.ogex");
+    auto& scene = g_pSceneManager->GetSceneForRendering();
 
     cout << "Dump of Cameras" << endl;
     cout << "---------------------------" << endl;
-    cout << pScene->Cameras << endl;
+    weak_ptr<SceneObjectCamera> pCamera = scene.GetFirstCamera();
+    while (auto pObj = pCamera.lock())
+    {
+        cout << *pObj << endl;
+        pCamera = scene.GetNextCamera();
+    }
 
     cout << "Dump of Lights" << endl;
     cout << "---------------------------" << endl;
-    cout << pScene->Lights << endl;
+    weak_ptr<SceneObjectLight> pLight = scene.GetFirstLight();
+    while (auto pObj = pLight.lock())
+    {
+        cout << *pObj << endl;
+        pLight = scene.GetNextLight();
+    }
 
     cout << "Dump of Geometries" << endl;
     cout << "---------------------------" << endl;
-    cout << pScene->Geometries << endl;
+    weak_ptr<SceneObjectGeometry> pGeometry = scene.GetFirstGeometry();
+    while (auto pObj = pGeometry.lock())
+    {
+        cout << *pObj << endl;
+        pGeometry = scene.GetNextGeometry();
+    }
 
     cout << "Dump of Materials" << endl;
     cout << "---------------------------" << endl;
-    cout << pScene->Materials << endl;
+    weak_ptr<SceneObjectMaterial> pMaterial = scene.GetFirstMaterial();
+    while (auto pObj = pMaterial.lock())
+    {
+        cout << *pObj << endl;
+        pMaterial = scene.GetNextMaterial();
+    }
 
+    g_pSceneManager->Finalize();
     g_pAssetLoader->Finalize();
     g_pMemoryManager->Finalize();
 
+    delete g_pSceneManager;
     delete g_pAssetLoader;
     delete g_pMemoryManager;
 
