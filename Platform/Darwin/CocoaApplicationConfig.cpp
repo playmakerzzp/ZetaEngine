e0e2067039c4699141b5ffc6d8896f0d93bed44f
playmaker <1344085659@qq.com>
2022-11-24 10:53:34 +0800

    update platform library

--- Platform/Darwin/CocoaApplicationConfig.cpp
+++ Platform/Darwin/CocoaApplicationConfig.cpp
@@ -1,12 +1,12 @@
-#include "BaseApplication.hpp"
-#include "GraphicsManager.hpp"
+#include "CocoaApplication.h"
 #include "MemoryManager.hpp"
+#include "GraphicsManager.hpp"
 #include "AssetLoader.hpp"
 #include "SceneManager.hpp"
 
 namespace ZetaEngine {
-    GfxConfiguration config;
-	IApplication*    g_pApp             = static_cast<IApplication*>(new BaseApplication(config));
+    GfxConfiguration config(8, 8, 8, 8, 24, 8, 0, 960, 540, "Game Engine From Scratch (MacOS Cocoa)");
+    IApplication* g_pApp                = static_cast<IApplication*>(new CocoaApplication(config));
     GraphicsManager* g_pGraphicsManager = static_cast<GraphicsManager*>(new GraphicsManager);
     MemoryManager*   g_pMemoryManager   = static_cast<MemoryManager*>(new MemoryManager);
     AssetLoader*     g_pAssetLoader     = static_cast<AssetLoader*>(new AssetLoader);
---
+++ Platform/Darwin/CocoaOpenGLApplication.mm
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <climits>
+#include <cstring>
+#include "CocoaOpenGLApplication.h"
+
+#import "GLView.h"
+
+using namespace ZetaEngine;
+
+int CocoaOpenGLApplication::Initialize()
+{
+    int result = 0;
+
+    result = CocoaApplication::Initialize();
+
+    if (!result) {
+        NSOpenGLPixelFormatAttribute attrs[] = {
+            NSOpenGLPFAAccelerated,
+            NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion3_2Core,
+            NSOpenGLPFAColorSize,32,
+            NSOpenGLPFADepthSize,24,
+            NSOpenGLPFAStencilSize,8,
+            NSOpenGLPFADoubleBuffer,
+            NSOpenGLPFAAccelerated,
+            NSOpenGLPFAMultisample,
+            NSOpenGLPFASampleBuffers,1,
+            NSOpenGLPFASamples,4, // 4x MSAA
+            0
+        };
+
+        GLView* pGLView = [GLView new];
+        pGLView.pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attrs];
+
+        if([pGLView pixelFormat] == nil)
+        {
+            NSLog(@"No valid matching OpenGL Pixel Format found");
+            [pGLView release];
+            return -1;
+        }
+
+        [pGLView initWithFrame:CGRectMake(0, 0, m_Config.screenWidth, m_Config.screenHeight)];
+
+        [m_pWindow setContentView:pGLView];
+    }
+
+    return result;
+}
+
+void CocoaOpenGLApplication::Finalize()
+{
+    CocoaApplication::Finalize();
+}
+
+void CocoaOpenGLApplication::Tick()
+{
+    CocoaApplication::Tick();
+    [[m_pWindow contentView] setNeedsDisplay:YES];
+}
+
