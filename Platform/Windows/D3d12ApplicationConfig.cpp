02d621519f6b414b06c9933b1fa5a14838394d94
playmaker <1344085659@qq.com>
2023-01-13 19:26:25 +0800

    update Windows Platform

--- Platform/Windows/D3d12ApplicationConfig.cpp
+++ Platform/Windows/D3d12ApplicationConfig.cpp
@@ -10,6 +10,8 @@ namespace ZetaEngine {
     AssetLoader*     g_pAssetLoader     = static_cast<AssetLoader*>(new AssetLoader);
     SceneManager*    g_pSceneManager    = static_cast<SceneManager*>(new SceneManager);
     InputManager*    g_pInputManager    = static_cast<InputManager*>(new InputManager);
-    PhysicsManager*  g_pPhysicsManager  = static_cast<PhysicsManager*>(new PhysicsManager);
+#ifdef DEBUG
+    DebugManager*    g_pDebugManager    = static_cast<DebugManager*>(new DebugManager);
+#endif
 }
 
