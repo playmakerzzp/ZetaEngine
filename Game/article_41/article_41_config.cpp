0d1447cfda3de124e358a751d2bee26b4394d4b0
playmaker <1344085659@qq.com>
2023-02-27 17:20:31 +0800

    delete 41

--- Game/article_41/article_41_config.cpp
+++
@@ -1,9 +0,0 @@
-#include "GfxConfiguration.hpp"
-#include "article_41_logic.hpp"
-#include "My/MyPhysicsManager.hpp"
-
-namespace ZetaEngine {
-    GfxConfiguration config(8, 8, 8, 8, 24, 8, 0, 960, 540, "article 41");
-    GameLogic*       g_pGameLogic       = static_cast<GameLogic*>(new article_41_logic);
-    IPhysicsManager*  g_pPhysicsManager  = static_cast<IPhysicsManager*>(new MyPhysicsManager);
-}
--- Game/article_43/article_43_config.cpp
+++
@@ -1,9 +0,0 @@
-#include "GfxConfiguration.hpp"
-#include "article_43_logic.hpp"
-#include "My/MyPhysicsManager.hpp"
-
-namespace ZetaEngine {
-    GfxConfiguration config(8, 8, 8, 8, 24, 8, 0, 960, 540, "article 43");
-    GameLogic*       g_pGameLogic       = static_cast<GameLogic*>(new article_43_logic);
-    IPhysicsManager*  g_pPhysicsManager  = static_cast<IPhysicsManager*>(new MyPhysicsManager);
-}
