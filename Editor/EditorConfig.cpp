e37edf8b8fdc8d38e87b5e5aa5283a201563442e
playmaker <1344085659@qq.com>
2023-03-11 16:31:31 +0800

    update Editor

--- Editor/EditorConfig.cpp
+++ Editor/EditorConfig.cpp
@@ -3,7 +3,7 @@
 #include "My/MyPhysicsManager.hpp"
 
 namespace ZetaEngine {
-    GfxConfiguration config(8, 8, 8, 8, 24, 8, 0, 960, 540, "GameEngineFromScratch Editor");
+    GfxConfiguration config(8, 8, 8, 8, 24, 8, 0, 1920, 1080, "ZetaEngine Editor");
     IGameLogic*       g_pGameLogic       = static_cast<IGameLogic*>(new EditorLogic);
     IPhysicsManager*  g_pPhysicsManager  = static_cast<IPhysicsManager*>(new MyPhysicsManager);
 }
